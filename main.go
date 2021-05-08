0af45052dc8b25e88068b96e4c053e4fde79629a
vele <m.hristov@executie.org>
2021-05-09 00:01:39 +0100

    Initial commit

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+
+	"github.com/vele/chia-console/chia"
+)
 
 var (
 	certFile = flag.String("cert", "someCertFile", "A PEM eoncoded certificate file.")
 	keyFile  = flag.String("key", "someKeyFile", "A PEM encoded private key file.")
 	caFile   = flag.String("CA", "someCertCAFile", "A PEM eoncoded CA's certificate file.")
 )
+
+func main() {
+	flag.Parse()
+	chiaClient := chia.NewClient(*certFile, *keyFile, *caFile)
+	fmt.Println(chiaClient)
+
+}
