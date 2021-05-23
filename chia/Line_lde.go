a9bd835677881eecb1a1a685da74a196f8d725d1
vele <m.hristov@executie.org>
2021-05-23 21:57:47 +0100

    added logfile parsing

--- chia/Line_lde.go
+++ chia/Line_lde.go
@@ -14,11 +14,10 @@ var constFoundSpace = []byte("Found ")
 var constHarvesterSpaceChiaDotHarvesterDotHarvesterColonSpaceINFOSpaces = []byte("harvester chia.harvester.harvester: INFO     ")
 var constPlotsSpaceWereSpaceEligibleSpaceForSpaceFarmingSpace = []byte("plots were eligible for farming ")
 var constProofsDotSpace = []byte("proofs. ")
+var constSDot = []byte("s.")
 var constSpace = []byte(" ")
-var constSpacePlots = []byte(" plots")
-var constSpaceSDot = []byte(" s.")
+var constSpaceTotalSpace = []byte(" Total ")
 var constTimeColonSpace = []byte("Time: ")
-var constTotalSpace = []byte("Total ")
 
 // filename: line.lde
 //2021-05-21T20:36:36.960 harvester chia.harvester.harvester: INFO     4 plots were eligible for farming 8b1b2e42f0... Found 0 proofs. Time: 0.71902 s. Total 1509 plots
@@ -124,31 +123,29 @@ func (p *Line) Extract(line []byte) (bool, error) {
 	if pos >= 0 {
 		p.ParseTime = p.Rest[:pos]
 		p.Rest = p.Rest[pos+len(constSpace):]
-
 	} else {
 		return false, nil
 	}
 
-	// Checks if the rest starts with `" s."` and pass it
-	if bytes.HasPrefix(p.Rest, constSpaceSDot) {
-		p.Rest = p.Rest[len(constSpaceSDot):]
-		fmt.Println("|321321321|")
+	// Checks if the rest starts with `"s."` and pass it
+	if bytes.HasPrefix(p.Rest, constSDot) {
+		p.Rest = p.Rest[len(constSDot):]
 	} else {
 		return false, nil
 	}
 
-	// Checks if the rest starts with `"Total "` and pass it
-	if bytes.HasPrefix(p.Rest, constTotalSpace) {
-		p.Rest = p.Rest[len(constTotalSpace):]
+	// Checks if the rest starts with `" Total "` and pass it
+	if bytes.HasPrefix(p.Rest, constSpaceTotalSpace) {
+		p.Rest = p.Rest[len(constSpaceTotalSpace):]
 	} else {
 		return false, nil
 	}
 
-	// Take until " plots" as PlotsCount(string)
-	pos = bytes.Index(p.Rest, constSpacePlots)
+	// Take until " " as PlotsCount(string)
+	pos = bytes.Index(p.Rest, constSpace)
 	if pos >= 0 {
 		p.PlotsCount = p.Rest[:pos]
-		p.Rest = p.Rest[pos+len(constSpacePlots):]
+		p.Rest = p.Rest[pos+len(constSpace):]
 	} else {
 		return false, nil
 	}
