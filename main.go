e134ec0786078699ec170db4d6db8e1fdf74fb1b
vele <m.hristov@executie.org>
2021-05-24 01:11:00 +0100

    added logfile parsing

--- main.go
+++ main.go
@@ -58,7 +58,7 @@ func main() {
 	for item := range fetchLogs {
 		parseTime, _ := time.Parse(time.RFC3339, string(fetchLogs[item].Time))
 		elapsedTime := time.Since(parseTime).Hours()
-		log.Printf("\n%f", elapsedTime)
+		log.Printf("\n%v", elapsedTime)
 
 	}
 	ChiaPlotsSparkline := w.NewPlot()
