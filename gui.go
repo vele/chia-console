a8215944b10e57bebd3a88342c5cc6c142643e5b
vele <m.hristov@executie.org>
2021-05-25 23:12:43 +0100

    added logfile parsing

--- gui.go
+++ gui.go
@@ -189,7 +189,7 @@ func onMovingCursorRedrawView(g *gocui.Gui, v *gocui.View) error {
 func middleTop(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
 	//int(float32(maxY) / 2)
-	if v, err := g.SetView("totalPlots", maxX/4+1, 0, maxX/2, int(float32(maxY)/2)); err != nil {
+	if v, err := g.SetView("totalPlots", maxX/4+1, 0, maxX/2, int(float32(maxY)/3)); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
