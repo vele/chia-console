3bc996c9a9c664fb5e9351a7188247ae13ffeb4c
vele <m.hristov@executie.org>
2021-05-25 20:45:22 +0100

    added logfile parsing

--- main.go
+++ main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"os"
-	"time"
 
-	ui "github.com/gizak/termui/v3"
-	w "github.com/gizak/termui/v3/widgets"
-	"github.com/vele/chia-console/chia"
+	"github.com/jroimartin/gocui"
 )
 
 var (
@@ -20,14 +16,8 @@ var (
 	harvesterCertFile = flag.String("harvesterCrt", "/root/.chia/mainnet/config/ssl/harvester/private_harvester.crt", "A PEM encoded private key file.")
 	harvesterKeyFile  = flag.String("harvesterKey", "/root/.chia/mainnet/config/ssl/harvester/private_harvester.key", "A PEM encoded private key file.")
 	caFile            = flag.String("CA", "/root/.chia/mainnet/config/ssl/ca/chia_ca.crt", "A PEM eoncoded CA's certificate file.")
-	logFile           = flag.String("LogFile", " /root/.chia/mainnet/log/debug.log", "The location of chia log files.")
+	logFile           = flag.String("LogFileDir", " /root/.chia/mainnet/log/debug.log", "The location of chia log files.")
 )
-
-type LogData struct {
-	PlotCounters  []float64
-	ProofCounters []float64
-	ParseTimes    []float64
-}
 
 func main() {
 	flag.Parse()
@@ -43,70 +33,22 @@ func main() {
 	os.Setenv("CHIA_HARVESTER_URL", "https://127.0.0.1:8560")
 	os.Setenv("CHIA_LOGFILE", *logFile)
 
-	if err := ui.Init(); err != nil {
-		log.Fatalf("failed to initialize termui: %v", err)
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
 	}
-	defer ui.Close()
+	defer g.Close()
 
-	grid := ui.NewGrid()
-	termWidth, termHeight := ui.TerminalDimensions()
-	grid.SetRect(0, 0, termWidth, termHeight)
+	g.Cursor = true
 
-	logCounters := PopulateLogData()
-	ChiaPlotsSparkline := w.NewSparkline()
-	ChiaPlotsSparkline.Data = logCounters.PlotCounters
-	ChiaPlotsSparkline.Title = "Plot counts"
-	ChiaPlotsSparkline.LineColor = ui.ColorYellow
-	ChiaProofsSparkline := w.NewSparkline()
-	ChiaProofsSparkline.Data = logCounters.ProofCounters
-	ChiaProofsSparkline.LineColor = ui.ColorGreen
-	ChiaTimesSparkline := w.NewSparkline()
-	ChiaTimesSparkline.Data = logCounters.ParseTimes
-	ChiaTimesSparkline.LineColor = ui.ColorBlue
+	g.SetManagerFunc(layout)
 
-	ChiaSparkilenGroup := w.NewSparklineGroup(ChiaPlotsSparkline, ChiaProofsSparkline, ChiaTimesSparkline)
-	ChiaSparkilenGroup.Title = "Eligable Plot Counts"
+	if err := keybindings(g); err != nil {
+		log.Panicln(err)
+	}
 
-	grid.Set(
-		ui.NewRow(1.0/2,
-			ui.NewCol(1.0/2, ChiaSparkilenGroup),
-		),
-	)
-	draw := func() {
-		logCounters := PopulateLogData()
-		ChiaPlotsSparkline.Data = logCounters.PlotCounters
-		ChiaProofsSparkline.Data = logCounters.ProofCounters
-		ChiaTimesSparkline.Data = logCounters.ParseTimes
-		ChiaSparkilenGroup.Title = fmt.Sprintf("Eligable Plot Counts %s ", time.Now().String())
-		ui.Render(grid)
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
 	}
-	draw()
-	uiEvents := ui.PollEvents()
-	ticker := time.NewTicker(time.Second * 5).C
-	for {
-		select {
-		case e := <-uiEvents:
-			switch e.ID {
-			case "q", "<C-c>":
-				return
-			}
-		case <-ticker:
-			draw()
-		}
-	}
-}
-func PopulateLogData() *LogData {
-	logs := chia.ParseLogs()
-	l := len(logs)
-	ld := &LogData{
-		PlotCounters:  make([]float64, l),
-		ProofCounters: make([]float64, l),
-		ParseTimes:    make([]float64, l),
-	}
-	for i, item := range logs {
-		ld.PlotCounters[i] = float64(item.Plots)
-		ld.ProofCounters[i] = float64(item.Proofs)
-		ld.ParseTimes[i] = item.ParseTime
-	}
-	return ld
+
 }
