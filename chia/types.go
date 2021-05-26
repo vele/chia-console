3901e639f4f726e07656dea86aae196ca7792d53
vele <m.hristov@executie.org>
2021-05-26 01:17:42 +0100

    added logfile parsing

--- chia/types.go
+++ chia/types.go
@@ -10,6 +10,39 @@ type ChiaBlockchainState struct {
 		GenesisChallengeInitialized bool `json:"genesis_challenge_initialized"`
 		MempoolSize                 int  `json:"mempool_size"`
 		Peak                        struct {
+			ChallengeBlockInfoHash string `json:"challenge_block_info_hash"`
+			ChallengeVdfOutput     struct {
+				Data string `json:"data"`
+			} `json:"challenge_vdf_output"`
+			Deficit                            int         `json:"deficit"`
+			FarmerPuzzleHash                   string      `json:"farmer_puzzle_hash"`
+			Fees                               int         `json:"fees"`
+			FinishedChallengeSlotHashes        interface{} `json:"finished_challenge_slot_hashes"`
+			FinishedInfusedChallengeSlotHashes interface{} `json:"finished_infused_challenge_slot_hashes"`
+			FinishedRewardSlotHashes           interface{} `json:"finished_reward_slot_hashes"`
+			HeaderHash                         string      `json:"header_hash"`
+			Height                             int         `json:"height"`
+			InfusedChallengeVdfOutput          struct {
+				Data string `json:"data"`
+			} `json:"infused_challenge_vdf_output"`
+			Overflow                   bool   `json:"overflow"`
+			PoolPuzzleHash             string `json:"pool_puzzle_hash"`
+			PrevHash                   string `json:"prev_hash"`
+			PrevTransactionBlockHash   string `json:"prev_transaction_block_hash"`
+			PrevTransactionBlockHeight int    `json:"prev_transaction_block_height"`
+			RequiredIters              int    `json:"required_iters"`
+			RewardClaimsIncorporated   []struct {
+				Amount         int64  `json:"amount"`
+				ParentCoinInfo string `json:"parent_coin_info"`
+				PuzzleHash     string `json:"puzzle_hash"`
+			} `json:"reward_claims_incorporated"`
+			RewardInfusionNewChallenge string      `json:"reward_infusion_new_challenge"`
+			SignagePointIndex          int         `json:"signage_point_index"`
+			SubEpochSummaryIncluded    interface{} `json:"sub_epoch_summary_included"`
+			SubSlotIters               int         `json:"sub_slot_iters"`
+			Timestamp                  int         `json:"timestamp"`
+			TotalIters                 int64       `json:"total_iters"`
+			Weight                     int         `json:"weight"`
 		} `json:"peak"`
 		Space        int64 `json:"space"`
 		SubSlotIters int   `json:"sub_slot_iters"`
