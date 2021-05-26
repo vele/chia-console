54f4b0639f93ffc1af87af5a894c34818ca20630
vele <m.hristov@executie.org>
2021-05-26 03:14:49 +0100

    added logfile parsing

--- gui.go
+++ gui.go
@@ -256,7 +256,7 @@ func mainLayout(g *gocui.Gui) error {
 		for item := range ok {
 			data = append(data, float64(ok[item].Plots))
 		}
-		graph := asciigraph.Plot(data, asciigraph.Height(15))
+		graph := asciigraph.Plot(data, asciigraph.Height(10))
 		fmt.Fprintln(v, graph)
 	}
 	if err := leftTop(g); err != nil {
