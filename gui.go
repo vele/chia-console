a819cb327587dad40d98f43c899c1da4613e42e0
vele <m.hristov@executie.org>
2021-05-27 22:23:50 +0100

    added logfile parsing

--- gui.go
+++ gui.go
@@ -167,7 +167,7 @@ func walletLayout(g *gocui.Gui) error {
 func plotsLayout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
 
-	if v, err := g.SetView("plots", maxX/2+1, maxY/4+1, int(float32(maxX)/1.8), int(float32(maxY)/2)); err != nil {
+	if v, err := g.SetView("plots", maxX/2+1, maxY/4+1, int(float32(maxX)/1.5), int(float32(maxY)/2)); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
@@ -185,7 +185,7 @@ func plotsLayout(g *gocui.Gui) error {
 }
 func priceLayout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("chia_price", int(float32(maxX)/1.8)+1, maxY/4+1, int(float32(maxX)/1.5), int(float32(maxY)/2)); err != nil {
+	if v, err := g.SetView("chia_price", int(float32(maxX)/1.5)+1, maxY/4+1, int(float32(maxX)/1.5), int(float32(maxY)/2)); err != nil {
 		if err != gocui.ErrUnknownView {
 			fmt.Println(int(float32(maxX)/1.2)+1, int(float32(maxX)/1.5))
 			return err
