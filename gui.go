ab1866af6262d9219243ed4f4827a918dc459a0a
vele <m.hristov@executie.org>
2021-05-29 01:28:52 +0100

    Added supported subnet/vpcs for DEV Env

--- gui.go
+++ gui.go
@@ -216,7 +216,7 @@ func walletLayout(g *gocui.Gui) error {
 func plotsLayout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
 
-	if v, err := g.SetView("plots", maxX/2+1, maxY/4+1, int(float32(maxX)/1.6), int(float32(maxY)/3)); err != nil {
+	if v, err := g.SetView("plots", maxX/2+1, maxY/4+1, int(float32(maxX)/1.2), int(float32(maxY)/3)); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
@@ -234,7 +234,7 @@ func plotsLayout(g *gocui.Gui) error {
 }
 func priceLayout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("chia_price", int(float32(maxX)/1.6)+1, maxY/4+1, maxX-1, int(float32(maxY)/3)); err != nil {
+	if v, err := g.SetView("chia_price", int(float32(maxX)/1.2)+1, maxY/4+1, maxX-1, int(float32(maxY)/3)); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
