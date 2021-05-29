2ae6ddf3e1ef73450e73bff4c0abd2c3d9347b36
vele <m.hristov@executie.org>
2021-05-29 01:57:59 +0100

    Added supported subnet/vpcs for DEV Env

--- gui.go
+++ gui.go
@@ -87,7 +87,7 @@ func updateChiaPriceGUI(g *gocui.Gui) error {
 				return err
 			}
 			v.Clear()
-			fmt.Fprintf(v, "Current chia price ( XCH ): \033[34m USD%f\033[0m \n", ok.ChiaPrice)
+			fmt.Fprintf(v, "Current chia price ( XCH ): \033[34mUSD%f\033[0m \n", ok.ChiaPrice)
 			isPositive1h := math.Signbit(ok.PercentChange1H)
 			if isPositive1h {
 				fmt.Fprintf(v, "\nCurrent chia price change 1 h( XCH ):\033[31m%f\033[0m \n", ok.PercentChange1H)
