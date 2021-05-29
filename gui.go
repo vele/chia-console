2b4b9fee48cf4d38a0e72172f7798bb290254362
vele <m.hristov@executie.org>
2021-05-29 01:26:49 +0100

    Added supported subnet/vpcs for DEV Env

--- gui.go
+++ gui.go
@@ -169,7 +169,7 @@ func keybindings(g *gocui.Gui) error {
 
 func detailsLayout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("blockchain_details", 0, maxY/4+1, maxX/4, int(float32(maxY)/2)); err != nil {
+	if v, err := g.SetView("blockchain_details", 0, maxY/4+1, maxX/4, int(float32(maxY)/3)); err != nil {
 
 		if err != gocui.ErrUnknownView {
 			return err
@@ -194,7 +194,7 @@ func detailsLayout(g *gocui.Gui) error {
 func walletLayout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
 
-	if v, err := g.SetView("wallet", maxX/4+1, maxY/4+1, maxX/2, int(float32(maxY)/2)); err != nil {
+	if v, err := g.SetView("wallet", maxX/4+1, maxY/4+1, maxX/2, int(float32(maxY)/3)); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
@@ -234,7 +234,7 @@ func plotsLayout(g *gocui.Gui) error {
 }
 func priceLayout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("chia_price", int(float32(maxX)/1.5)+1, maxY/4+1, maxX-1, int(float32(maxY)/2)); err != nil {
+	if v, err := g.SetView("chia_price", int(float32(maxX)/1.5)+1, maxY/4+1, maxX-1, int(float32(maxY)/3)); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
