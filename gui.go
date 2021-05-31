1986c2200e7abcdea4f2098cd556a1126d7053d0
vele <m.hristov@executie.org>
2021-06-01 00:23:06 +0100

    Added supported subnet/vpcs for DEV Env

--- gui.go
+++ gui.go
@@ -207,7 +207,7 @@ func banner(g *gocui.Gui) error {
 }
 func leftTop(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("diskspace", 0, 0, int(float32(maxX)/4), int(float32(maxY)/4)); err != nil {
+	if v, err := g.SetView("diskspace", 0, 0, int(float32(maxX)/3), int(float32(maxY)/4)); err != nil {
 		if err != gocui.ErrUnknownView {
 			log.Fatal("POOP")
 			return err
@@ -222,7 +222,7 @@ func leftTop(g *gocui.Gui) error {
 func secondRowGraph(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
 	//int(float32(maxY) / 2)
-	if v, err := g.SetView("totalPlots", int(float32(maxX)/3)+1, maxY/4+1, maxX-10, int(float32(maxY)/2)); err != nil {
+	if v, err := g.SetView("totalPlots", int(float32(maxX)/3)+1, maxY/4+1, maxX-1, int(float32(maxY)/2)); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
@@ -235,7 +235,7 @@ func secondRowGraph(g *gocui.Gui) error {
 }
 func firstRowGraph(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("main", int(float32(maxX)/3)+1, 0, maxX-10, maxY/4); err != nil {
+	if v, err := g.SetView("main", int(float32(maxX)/3)+1, 0, maxX-1, maxY/4); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
