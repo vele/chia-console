f22fb2eb0a6b9029ede695d45bfe77d7e4b90c8e
vele <m.hristov@executie.org>
2021-05-31 15:16:47 +0100

    Added supported subnet/vpcs for DEV Env

--- gui.go
+++ gui.go
@@ -144,7 +144,7 @@ func drawFreeSpaceTable(g *gocui.Gui) error {
 		totalFreeSpace := diskInfoS5.TotalFreeSpaceBytes + diskInfoS4.TotalFreeSpaceBytes + diskInfoS2.TotalFreeSpaceBytes + diskInfoS1.TotalFreeSpaceBytes
 		table.SetHeader([]string{"Part", "Total Disk Space", "Total Free Space", "Util %"})
 		table.SetFooter([]string{"Tot", humanize.Bytes(totalDiskSpace), humanize.Bytes(totalFreeSpace), ""})
-		table.SetBorder(false) // Set Border to false
+		table.SetBorder(true) // Set Border to false
 		//table.AppendBulk(data) // Add Bulk Data
 		table.Render()
 		g.Update(func(g *gocui.Gui) error {
