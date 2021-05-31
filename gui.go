94782a5d444c383d2bcbd456e6f29edb3c02c595
vele <m.hristov@executie.org>
2021-05-31 15:30:10 +0100

    Added supported subnet/vpcs for DEV Env

--- gui.go
+++ gui.go
@@ -174,7 +174,7 @@ func keybindings(g *gocui.Gui) error {
 
 func detailsLayout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("blockchain_details", 0, maxY/4+1, maxX/4, int(float32(maxY)/3)); err != nil {
+	if v, err := g.SetView("blockchain_details", 0, maxY/4+1, maxX/4, int(float32(maxY)/2)); err != nil {
 
 		if err != gocui.ErrUnknownView {
 			return err
@@ -199,7 +199,7 @@ func detailsLayout(g *gocui.Gui) error {
 func walletLayout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
 
-	if v, err := g.SetView("wallet", maxX/4+1, maxY/4+1, maxX/2, int(float32(maxY)/3)); err != nil {
+	if v, err := g.SetView("wallet", maxX/4+1, maxY/4+1, maxX/2, int(float32(maxY)/2)); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
@@ -221,7 +221,7 @@ func walletLayout(g *gocui.Gui) error {
 func plotsLayout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
 
-	if v, err := g.SetView("plots", maxX/2+1, maxY/4+1, int(float32(maxX)/1.5), int(float32(maxY)/3)); err != nil {
+	if v, err := g.SetView("plots", maxX/2+1, maxY/4+1, int(float32(maxX)/1.5), int(float32(maxY)/2)); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
@@ -239,7 +239,7 @@ func plotsLayout(g *gocui.Gui) error {
 }
 func priceLayout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("chia_price", int(float32(maxX)/1.5)+1, maxY/4+1, int(float32(maxX)-1.5), int(float32(maxY)/3)); err != nil {
+	if v, err := g.SetView("chia_price", int(float32(maxX)/1.5)+1, maxY/4+1, int(float32(maxX)-1.5), int(float32(maxY)/2)); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
@@ -251,7 +251,7 @@ func priceLayout(g *gocui.Gui) error {
 }
 func banner(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("banner", 0, int(float32(maxY)/2.5)+1, maxX-1, int(float32(maxY)/2)); err != nil {
+	if v, err := g.SetView("banner", 0, int(float32(maxY)/2)+1, maxX-1, int(float32(maxY)/2)); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
