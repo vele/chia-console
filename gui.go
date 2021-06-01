e4ea1411a5c179984e29da9b78afe491ca74728c
vele <m.hristov@executie.org>
2021-06-01 14:25:27 +0100

    added logfile parsing

--- gui.go
+++ gui.go
@@ -119,7 +119,7 @@ func drawFreeSpaceTable(g *gocui.Gui) error {
 			chia_mojo_balance_spendable, _ := new(big.Float).SetPrec(precision).SetString(fmt.Sprintf("%d", wallet.WalletBalance.SpendableBalance))
 			formula_result_spendable := new(big.Float).Quo(chia_mojo_balance_spendable, chia_mojo_calc)
 			fmt.Fprintln(v, float64((len(res.Plots) * 102 / 1024)))
-			fmt.Fprintln(v, float64(int(returnBlockChainDetails()))/float64(1073741824))
+			fmt.Fprintln(v, float64(returnBlockChainDetails())/float64(1073741824))
 			chia_probability_formula := float64(1 - math.Pow(float64((len(res.Plots)*102/1024))/float64(int(returnBlockChainDetails()))/float64(1073741824), float64(4608)))
 			fmt.Fprintf(v, "\u2705 Current wallet ballance : %0.12f  \n", formula_result)
 			fmt.Fprintf(v, "\u2705 Spendable wallet ballance: %0.12f  \n", formula_result_spendable)
